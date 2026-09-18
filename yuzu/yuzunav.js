16be17d3c028e4bb442f0d8af6f688e78343a0ee
wyhlele <896756054@qq.com>
2026-09-19 00:21:16 +0800

    20260919

--- yuzu/yuzunav.js
+++ yuzu/yuzunav.js
@@ -183,11 +183,11 @@
                 folder: 'ten_sz',
                 expanded: false,
                 projects: [
-                    // { name: 'FUN FUN RE-BOOT', file: 'yuzu12op.html' },
+                    { name: 'FUN FUN RE-BOOT', file: 'yuzu12op.html' },
                     { name: 'Call My Name', file: 'yuzu12ednoa.html' },
                     { name: '君と僕のハジメテ', file: 'yuzu12noa.html' },
                     { name: 'ねえ。', file: 'yuzu12edamane.html' },
-                    // { name: 'ワタシだけ。', file: 'yuzu12amane.html' },
+                    { name: 'ワタシだけ。', file: 'yuzu12amane.html' },
                     { name: '以心伝心ジャーニー', file: 'yuzu12edkurumi.html' },
                     { name: 'アオハルデイズ', file: 'yuzu12kurumi.html' },
                     { name: 'DIVE', file: 'yuzu12edkaguya.html' },
